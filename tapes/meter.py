53b5b8f175f8517d7570a1456b037c018283373f
emilssolmanis <emils.solmanis@gmail.com>
2015-01-31 00:14:57 +0000

    Implement Meter class

--- tapes/meter.py
+++ tapes/meter.py
@@ -1,14 +1,55 @@
+from __future__ import division
+from time import time
+from tapes.average import EWMA
+
+
+_INTERVAL = 5.0
+
+
 class Meter(object):
     def __init__(self):
+        self.last_tick = time()
         self.count = 0
+        self.m1 = EWMA.one()
+        self.m5 = EWMA.five()
+        self.m15 = EWMA.fifteen()
 
-    def mark(self):
-        self.count += 1
+    def mark(self, n=1):
+        self._tick_if_needed()
+        self.count += n
+        self.m1.update(n)
+        self.m5.update(n)
+        self.m15.update(n)
+
+    def _get_one_minute(self):
+        self._tick_if_needed()
+        return self.m1.get_rate()
+
+    def _get_five_minute(self):
+        self._tick_if_needed()
+        return self.m5.get_rate()
+
+    def _get_fifteen_minute(self):
+        self._tick_if_needed()
+        return self.m15.get_rate()
+
+    def _tick_if_needed(self):
+        old_tick = self.last_tick
+        new_tick = time()
+        age = new_tick - old_tick
+        if age > _INTERVAL:
+            new_interval_start_tick = new_tick - age % _INTERVAL
+            self.last_tick = new_interval_start_tick
+            required_ticks = int(age // _INTERVAL)
+            for _ in range(required_ticks):
+                self.m1.tick()
+                self.m5.tick()
+                self.m15.tick()
 
     def get_values(self):
         return {
             'count': self.count,
-            'm1': 0.0,
-            'm5': 0.0,
-            'm15': 0.0,
+            'm1': self._get_one_minute(),
+            'm5': self._get_five_minute(),
+            'm15': self._get_fifteen_minute(),
         }
--- tests/test_meter.py
+++ tests/test_meter.py
@@ -1,4 +1,5 @@
 import unittest
+import fudge
 from tapes.registry import Registry
 
 
@@ -6,7 +7,22 @@ class MeterTestCase(unittest.TestCase):
     def setUp(self):
         self.registry = Registry()
 
-    def test_meter_count_works(self):
+    @fudge.patch('tapes.meter.time')
+    def test_meter_count_works(self, time):
+        (time
+         # initialize Meter
+         .expects_call().returns(1.0)
+         # mark()
+         .next_call().returns(6.0)
+         # mark()
+         .next_call().returns(12.0)
+         # mark()
+         .next_call().returns(20.0)
+         # get_stats()
+         .next_call().returns(20.0)
+         .next_call().returns(20.0)
+         .next_call().returns(20.0))
+
         meter = self.registry.meter('MeterTestCase.test_meter_count_works')
 
         meter.mark()
@@ -14,4 +30,8 @@ class MeterTestCase(unittest.TestCase):
         meter.mark()
 
         stats = self.registry.get_stats()
+
         assert stats.MeterTestCase.test_meter_count_works.count == 3
+        assert stats.MeterTestCase.test_meter_count_works.m1 > 0
+        assert stats.MeterTestCase.test_meter_count_works.m5 > 0
+        assert stats.MeterTestCase.test_meter_count_works.m15 > 0
