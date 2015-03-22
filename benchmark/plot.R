974e782f8f2986199b89795c95a631b6e894643d
emilssolmanis <emils.solmanis@gmail.com>
2015-03-22 20:57:41 +0000

    Add benchmarks to tox and docs

--- benchmark/plot.R
+++ benchmark/plot.R
@@ -4,44 +4,52 @@ args <- commandArgs(trailingOnly = TRUE);
 
 framework <- args[1];
 benchmark_type <- args[2];
-
+output_prefix <- args[3]
 
 no_metrics <- read.csv(
-    sprintf("benchmark_results/gp/benchmark.%s.no_metrics_%s_process.tsv", framework, benchmark_type),
+    sprintf("%s/benchmark_results/gp/benchmark.%s.no_metrics_%s_process.tsv", output_prefix, framework, benchmark_type),
     sep="\t"
 );
 
 scales_metrics <- read.csv(
-    sprintf("benchmark_results/gp/benchmark.%s.scales_metrics_%s_process.tsv", framework, benchmark_type),
+    sprintf("%s/benchmark_results/gp/benchmark.%s.scales_metrics_%s_process.tsv", output_prefix, framework, benchmark_type),
     sep="\t"
 );
 
 tapes_metrics <- read.csv(
-    sprintf("benchmark_results/gp/benchmark.%s.metrics_%s_process.tsv", framework, benchmark_type),
+    sprintf("%s/benchmark_results/gp/benchmark.%s.metrics_%s_process.tsv", output_prefix, framework, benchmark_type),
     sep="\t"
 );
 
 tapes_zmq_metrics <- read.csv(
-    sprintf("benchmark_results/gp/benchmark.%s.zmq_metrics_%s_process.tsv", framework, benchmark_type),
+    sprintf("%s/benchmark_results/gp/benchmark.%s.zmq_metrics_%s_process.tsv", output_prefix, framework, benchmark_type),
     sep="\t"
 );
 
+# drop the anomalies, especially for pypy
+dropoff_coeff <- 0.99
+
+no_metrics_times <- no_metrics$ttime[1:(length(no_metrics$ttime) * dropoff_coeff)]
+scales_metrics_times <- scales_metrics$ttime[1:(length(scales_metrics$ttime) * dropoff_coeff)]
+tapes_metrics_times <- tapes_metrics$ttime[1:(length(tapes_metrics$ttime) * dropoff_coeff)]
+tapes_zmq_metrics_times <- tapes_zmq_metrics$ttime[1:(length(tapes_zmq_metrics$ttime) * dropoff_coeff)]
+
 df <- data.frame(
     framework = factor(c(
-        rep("No metrics", length(no_metrics$ttime)),
-        rep("Scales", length(scales_metrics$ttime)),
-        rep("Tapes, vanilla", length(tapes_metrics$ttime)),
-        rep("Tapes, 0MQ", length(tapes_zmq_metrics$ttime))
+        rep("No metrics", length(no_metrics_times)),
+        rep("Scales", length(scales_metrics_times)),
+        rep("Tapes, vanilla", length(tapes_metrics_times)),
+        rep("Tapes, 0MQ", length(tapes_zmq_metrics_times))
     )),
-    times = c(no_metrics$ttime, scales_metrics$ttime, tapes_metrics$ttime, tapes_zmq_metrics$ttime)
+    times = c(no_metrics_times, scales_metrics_times, tapes_metrics_times, tapes_zmq_metrics_times)
 );
 
 # candle plot
-png(sprintf("benchmark_results/png/%s_%s_box.png", framework, benchmark_type), width=1920, height=1080);
+png(sprintf("%s/benchmark_results/png/%s_%s_box.png", output_prefix, framework, benchmark_type), width=800, height=450);
 ggplot(df, aes(x=framework, y=times, fill=framework)) + geom_boxplot();
 dev.off();
 
 # overlayed histograms
-png(sprintf("benchmark_results/png/%s_%s_hist.png", framework, benchmark_type), width=1920, height=1080);
+png(sprintf("%s/benchmark_results/png/%s_%s_hist.png", output_prefix, framework, benchmark_type), width=800, height=450);
 ggplot(df, aes(x=times, fill=framework)) + geom_density(alpha=.3)
 dev.off()
--- docs/index.rst
+++ docs/index.rst
@@ -8,9 +8,19 @@ Contents:
 
    quickstart
    api
+   benchmarks
 
 This is a native Python metrics library implementation. It currently supports Python 2.7, 3.4 and PyPy, but most other
 versions should probably work.
+
+Compared to `other libraries <https://github.com/Cue/scales>`_, tapes
+ - doesn't use a separate thread to decay the moving average's weights
+ - doesn't use "clever tricks" with stack frames, which means it also doesn't break in weird
+   ways (e.g., when used within a method decorated with ``@staticmethod``)
+ - has clean code, that passes ``flake8`` validation
+ - is also performance-oriented
+
+You can check the :doc:`benchmarks` yourself!
 
 Indices and tables
 ==================
